62375f48398607c3174eff041b694215798e27f3
emmanuel <emmanuel.a@lycee-de-beauvoir.education>
2025-03-10 11:33:52 +0100

    envoie du code traitement_calculatrice

---
+++ traitement_calculatrice.php
@@ -0,0 +1,31 @@
+<?php
+// Récupération des valeurs saisies dans le formulaire
+$chiffre1 = isset($_POST['saisi1']) ? $_POST['saisi1'] : 0;
+$chiffre2 = isset($_POST['saisi2']) ? $_POST['saisi2'] : 0;
+
+if (!is_numeric($chiffre1) || !is_numeric($chiffre2)) {
+    echo "Veuillez entrer des nombres valides dans les deux champs.";
+    exit;
+}
+
+// Initialisation de la variable $resultat
+$resultat = 0;
+
+// Vérification de l'opération à effectuer
+if (isset($_POST['plus'])) {
+    $resultat = $chiffre1 + $chiffre2;
+} elseif (isset($_POST['moins'])) {
+    $resultat = $chiffre1 - $chiffre2;
+} elseif (isset($_POST['multiplie'])) {
+    $resultat = $chiffre1 * $chiffre2;
+} elseif (isset($_POST['divise'])) {
+    if ($chiffre2 == 0) {
+        echo "Erreur : Division par zéro!";
+        exit;
+    }
+    $resultat = $chiffre1 / $chiffre2;
+}
+
+// Affichage du résultat
+echo "Le resultat de l'operation est : " . $resultat;
+?>
